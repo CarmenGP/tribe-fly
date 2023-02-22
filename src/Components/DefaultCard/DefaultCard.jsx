d7e760df5e1ab6f1ddcdbbf1f8d10e2d4f8df5ec
Bubbl3Waffl3 <salahlfd0@gmail.com>
2023-02-22 17:53:00 +0100

    ab

--- src/Components/DefaultCard/DefaultCard.jsx
+++ src/Components/DefaultCard/DefaultCard.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
+import './DefaultCard.css';
 
-function DefaultCard({ type, info}) {
-  return (
-    <div>
 
-    </div>
-  )
+function DefaultCard({ type, info }) {
+    return (<div>
+        
+    </div>)
 }
 
 export default DefaultCard
