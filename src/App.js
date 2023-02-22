964b85f6a5806d9739278ccaf656400f02d1a99a
JasMarin <yasminamarin17@gmail.com>
2023-02-22 19:28:37 +0100

    slider

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import { FooterContainer } from './Components/Footer/Footer';
 import WeatherPanel from './Components/Tiempo/WeatherPanel';
+import DarkVariantExample from './Components/Slider/slider';
 
 function App() {
   return (
     <div className="App">
-    
+      <DarkVariantExample/>
       <WeatherPanel />
       <FooterContainer /> 
     </div>
