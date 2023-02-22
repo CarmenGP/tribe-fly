573fd13db0728a2346455fa9fb77dd4e39a0dd15
Bubbl3Waffl3 <salahlfd0@gmail.com>
2023-02-22 18:24:13 +0100

    defaultCard sample done with css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
+import DefaultCard from './Components/DefaultCard/DefaultCard';
 import { FooterContainer } from './Components/Footer/Footer';
 import WeatherPanel from './Components/Tiempo/WeatherPanel';
 
 function App() {
   return (
     <div className="App">
-    
+      <DefaultCard type="category" info="Pueblos Rurales" img="https://i.gyazo.com/123ef1129e9399f09e45466a6123e9d4.jpg"/>
       <WeatherPanel />
       <FooterContainer /> 
     </div>
--- src/Components/DefaultCard/DefaultCard.jsx
+++ src/Components/DefaultCard/DefaultCard.jsx
@@ -2,9 +2,19 @@ import React from 'react'
 import './DefaultCard.css';
 
 
-function DefaultCard({ type, info }) {
-    return (<div>
-        
+function DefaultCard({ type, info, img }) {
+    return (type == "category" ?
+    <div className="card" style={{width: "20rem"}}>
+    <div className="card-img-caption">
+      <p className="card-text">{info}</p>
+      <img className="card-img-top" src={img} alt="Card image cap" />
+    </div>
+  </div>
+    :
+    <div>
+        <img src={img} alt="" />
+        <h2>{info.title}</h2>
+        <p>{info.description}</p>
     </div>)
 }
 
