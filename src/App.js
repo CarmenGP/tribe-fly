3b1b6fe278d929992b5deab9a7590ad9e4cd2dd3
LindaHG1 <116809226+LindaHG1@users.noreply.github.com>
2023-02-22 13:32:36 +0100

    bootstrap add

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import WeatherPanel from './Components/Tiempo/WeatherPanel';
+
+function App() {
+  return (
+    <div className="App">
+    
+      <WeatherPanel /> 
+    </div>
+  );
+}
+
+export default App;
