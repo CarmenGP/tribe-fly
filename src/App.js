1274822d99f17eeb5c866aba81a0d8026aa1576a
Bubbl3Waffl3 <salahlfd0@gmail.com>
2023-02-23 12:58:47 +0100

    marketplace page added and search bar with searching functionality done

--- src/App.js
+++ src/App.js
@@ -1,20 +1,16 @@
 import './App.css';
 import DefaultCard from './Components/DefaultCard/DefaultCard';
 import { FooterContainer } from './Components/Footer/Footer';
+import MarketplacePage from './Components/MarketplacePage/MarketplacePage';
 import WeatherPanel from './Components/Tiempo/WeatherPanel';
 
 function App() {
-  const arr = [{
-    title: "Barcelona",
-    description: "Tour de una tarde por el centro"
-  }, {
-    title: "Valencia",
-    description: "4 días del 1/03 - 4/03 (Fallas)"
-  }]
+  
 
   return (
+
     <div className="App">
-      {arr.map(item => <DefaultCard  type="product" info={item} img="https://i.gyazo.com/123ef1129e9399f09e45466a6123e9d4.jpg"/>)}
+      <MarketplacePage />
       <WeatherPanel />
       <FooterContainer /> 
     </div>
--- src/Components/DefaultCard/DefaultCard.jsx
+++ src/Components/DefaultCard/DefaultCard.jsx
@@ -4,25 +4,23 @@ import './DefaultCard.css';
 
 function DefaultCard({ type, info, img }) {
     return (type == "category" ?
-        <div className="card" style={{ width: "20rem" }}>
+        <div className="card mb-4 mb-md-0" style={{ width: "20rem" }}>
             <div className="card-img-caption">
-                <p className="card-text h4">{info}</p>
+                <p className="card-text h4">{info.title}</p>
                 <button className='card-button btn btn-light'>Descubrir</button>
                 <img className="card-img-top" src={img} alt="Card image cap" />
-                
+
             </div>
         </div>
         :
-        <div>
-            <div className="card" style={{ width: "20rem" }}>
-            <div className="card-img-caption">
-                <p className="card-text h4">{info.title}</p>
-                <p className="card-subText h6">{info.description}</p>
-                <button className='card-button btn btn-light'>Descubrir</button>
-                <img className="card-img-top" src={img} alt="Card image cap" />
-            </div>
-        </div>
-        </div>)
+            <div className="card mb-4" style={{ width: "20rem" }}>
+                <div className="card-img-caption">
+                    <p className="card-text h4">{info.title}</p>
+                    <p className="card-subText h6">{info.plan}</p>
+                    <button className='card-button btn btn-light'>Descubrir</button>
+                    <img className="card-img-top" src={img} alt="Card image cap" />
+                </div>
+            </div>)
 }
 
 export default DefaultCard
