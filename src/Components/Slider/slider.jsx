8cc6b47268c897996b56ba459a484989eba51668
JasMarin <yasminamarin17@gmail.com>
2023-02-23 12:45:19 +0100

    slider

--- src/Components/Slider/slider.jsx
+++ src/Components/Slider/slider.jsx
@@ -1,46 +1,36 @@
 import React from "react";
 import Carousel from "react-bootstrap/Carousel";
-
+import "./slider.css"
 function DarkVariantExample() {
     return (
-        <Carousel variant="dark">
-            <Carousel.Item>
+        <Carousel variant="dark" >
+            <Carousel.Item >
                 <img
-                    className="d-block w-100"
-                    src="holder.js/800x400?text=First slide&bg=f5f5f5"
-                    alt="First slide"
-                />
-                <Carousel.Caption>
-                    <h5>First slide label</h5>
-                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                </Carousel.Caption>
+                    className="slider"
+                    src="https://img.freepik.com/foto-gratis/arbol-cartel-excursion-senderismo-primavera-carpatos-rumania-grupo-personas-mochilas_1268-19949.jpg?w=900&t=st=1677142615~exp=1677143215~hmac=706e5808c82c1bac3a0977b305c6f9c5bea5153958b2de9276b21a7baea5bf80" class="" alt="senderismo"/>
+
             </Carousel.Item>
-            <Carousel.Item>
+            <Carousel.Item className="carousel-item" >
+                
                 <img
-                    className="d-block w-100"
-                    src="holder.js/800x400?text=Second slide&bg=eee"
-                    alt="Second slide"
+                    className="slider"
+                    src="https://img.freepik.com/foto-gratis/muchacha-hermosa-que-camina-calle-ciudad-viajando-tailandia_1150-12538.jpg?w=740&t=st=1677150727~exp=1677151327~hmac=79eb2b31b0ef8dbde7d1f0b5525ac33d694f4048c2155fa50668753f9ba57439"
+                    alt="barcelona"
                 />
-                <Carousel.Caption>
-                    <h5>Second slide label</h5>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                </Carousel.Caption>
+
             </Carousel.Item>
-            <Carousel.Item>
+            <Carousel.Item className="carousel-item">
+                
                 <img
-                    className="d-block w-100"
-                    src="holder.js/800x400?text=Third slide&bg=e5e5e5"
-                    alt="Third slide"
+                        className="slider"
+                    src="https://img.freepik.com/foto-gratis/grupo-jovenes-felices-cenando-vino_23-2148454139.jpg?w=996&t=st=1677144713~exp=1677145313~hmac=ad5b1f1b5edd99f021dcede2b4a1a39e7a1b35a42f3b2f2a638fa63451b8b772"
+                    alt="amigos"
                 />
-                <Carousel.Caption>
-                    <h5>Third slide label</h5>
-                    <p>
-                        Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-                    </p>
-                </Carousel.Caption>
+
             </Carousel.Item>
         </Carousel>
     );
 }
+
 
 export default DarkVariantExample;
