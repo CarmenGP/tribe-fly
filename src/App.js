e931732c130635b1caf2c56ccb476c34d208fd4e
Bubbl3Waffl3 <salahlfd0@gmail.com>
2023-02-23 08:55:29 +0100

    defaultCard category type styles added

--- src/App.js
+++ src/App.js
@@ -4,9 +4,17 @@ import { FooterContainer } from './Components/Footer/Footer';
 import WeatherPanel from './Components/Tiempo/WeatherPanel';
 
 function App() {
+  const arr = [{
+    title: "Barcelona",
+    description: "Tour de una tarde por el centro"
+  }, {
+    title: "Valencia",
+    description: "4 días del 1/03 - 4/03 (Fallas)"
+  }]
+
   return (
     <div className="App">
-      <DefaultCard type="category" info="Pueblos Rurales" img="https://i.gyazo.com/123ef1129e9399f09e45466a6123e9d4.jpg"/>
+      {arr.map(item => <DefaultCard  type="product" info={item} img="https://i.gyazo.com/123ef1129e9399f09e45466a6123e9d4.jpg"/>)}
       <WeatherPanel />
       <FooterContainer /> 
     </div>
--- src/Components/DefaultCard/DefaultCard.jsx
+++ src/Components/DefaultCard/DefaultCard.jsx
@@ -4,18 +4,25 @@ import './DefaultCard.css';
 
 function DefaultCard({ type, info, img }) {
     return (type == "category" ?
-    <div className="card" style={{width: "20rem"}}>
-    <div className="card-img-caption">
-      <p className="card-text">{info}</p>
-      <img className="card-img-top" src={img} alt="Card image cap" />
-    </div>
-  </div>
-    :
-    <div>
-        <img src={img} alt="" />
-        <h2>{info.title}</h2>
-        <p>{info.description}</p>
-    </div>)
+        <div className="card" style={{ width: "20rem" }}>
+            <div className="card-img-caption">
+                <p className="card-text h4">{info}</p>
+                <button className='card-button btn btn-light'>Descubrir</button>
+                <img className="card-img-top" src={img} alt="Card image cap" />
+                
+            </div>
+        </div>
+        :
+        <div>
+            <div className="card" style={{ width: "20rem" }}>
+            <div className="card-img-caption">
+                <p className="card-text h4">{info.title}</p>
+                <p className="card-subText h6">{info.description}</p>
+                <button className='card-button btn btn-light'>Descubrir</button>
+                <img className="card-img-top" src={img} alt="Card image cap" />
+            </div>
+        </div>
+        </div>)
 }
 
 export default DefaultCard
