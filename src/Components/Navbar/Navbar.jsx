171a8a8ec47ff53fc93ae57062ee5a7fc04d1404
LindaHG1 <116809226+LindaHG1@users.noreply.github.com>
2023-02-23 15:08:28 +0100

    Navbar added

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -3,6 +3,7 @@ import './Navbar.css';
 import { Icon } from "react-icons-kit";
 import { menu } from "react-icons-kit/feather/menu";
 import { x } from "react-icons-kit/feather/x";
+import logo from "./image/logo.png"
 
 export const Navbar = () => {
   const [toggle, setToggle] = useState(false);
@@ -13,14 +14,15 @@ export const Navbar = () => {
 
   return (
     <nav className={toggle ? "navbar expanded" : "navbar"}>
-        <img className="logo" src="/Navbar/logo.png" alt="Tribe.fly" />
+        <img className="logo" src={logo} alt="tribe.fly" />
       <div className="toggle-icon" onClick={handleToggle}>
         {toggle ? <Icon icon={x} size={28} /> : <Icon icon={menu} size={28} />}
       </div>
       <ul className="links">
-        <li>Home</li>
-        <li>Contactanos</li>       
-        <li>Contacto</li>
+        <li>Descúbre</li>
+        <li>Sobre nosotros</li>
+        <li>El tiempo</li>
+        <li>Contáctanos</li>
       </ul>
     </nav>
   );
