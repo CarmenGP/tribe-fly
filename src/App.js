037be7b2d93a24581be7fa55a96513adbd485e05
LindaHG1 <116809226+LindaHG1@users.noreply.github.com>
2023-02-23 17:00:32 +0100

    Color Changed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import './App.css';
 import AboutUs from './Components/AboutUs/AboutUs';
 import { FooterContainer } from './Components/Footer/Footer';
-import DarkVariantExample from './Components/Slider/slider';
 import WeatherPanel from './Components/Weather/WeatherPanel';
 import DarkVariantExample from './Components/Slider/slider'
 import Navbar from './Components/Navbar/Navbar';
