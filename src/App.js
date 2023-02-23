9ff9bc6a03ab6a3a5aacabd60a86a2d516df524c
vanessa.cp <vanehom@hotmail.com>
2023-02-23 13:01:51 +0100

    add about us

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
+import AboutUs from './Components/AboutUs/AboutUs';
 import { FooterContainer } from './Components/Footer/Footer';
 import WeatherPanel from './Components/Tiempo/WeatherPanel';
 
 function App() {
   return (
     <div className="App">
-    
+      <AboutUs/>
       <WeatherPanel />
       <FooterContainer /> 
     </div>
