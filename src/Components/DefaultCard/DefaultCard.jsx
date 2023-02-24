05ceb2958daa0da5296b5629ae1c50fd7157d99b
Bubbl3Waffl3 <salahlfd0@gmail.com>
2023-02-24 02:06:44 +0100

    detailsPage style done

--- src/Components/DefaultCard/DefaultCard.jsx
+++ src/Components/DefaultCard/DefaultCard.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
+import { useNavigate } from 'react-router'
 import "./DefaultCard.css"
 
 function DefaultCard({ type, info, img }) {
+    let navigate = useNavigate()
+
+
     return (type == "category" ?
         <div className="card mb-4 mb-md-0" style={{ width: "20rem" }}>
             <div className="card-img-caption">
@@ -16,7 +20,7 @@ function DefaultCard({ type, info, img }) {
             <div className="card-img-caption">
                 <p className="card-text h4">{info.title}</p>
                 <p className="card-subText h6">{info.plan}</p>
-                <button className='card-button btn btn-light'>Descubrir</button>
+                <button className='card-button btn btn-light' onClick={() => { navigate("/Detalles", { state: info }) }}>Descubrir</button>
                 <img className="card-img-top" src={img} alt="Card image cap" />
             </div>
         </div>)
--- src/Components/DetailsPage/DetailsPage.jsx
+++ src/Components/DetailsPage/DetailsPage.jsx
@@ -3,25 +3,27 @@ import bcn from '../DetailsPage/images/bcn.jpg'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Card } from 'react-bootstrap';
 import "./DetailsPage.css"
+import { useLocation } from 'react-router';
 
 function DetailsPage() {
-    
+  const stateInfo = useLocation().state
+
   return (
-    <Card>
-      <Card.Body>
-        <div className="row">
+    <div className='detailsPage d-flex container justify-content-md-around flex-column align-items-center'>
+      <Card>
+        <Card.Body>
+        <div className="row col-md-12 align-items-center">
           <div className="col-md-4">
             <Card.Img src={bcn} />
           </div>
           <div className="col-md-8">
-            <Card.Title>Barcelona, España</Card.Title>
-            <Card.Text>
-              Explora Barcelona y su cultura desde lo más profundo con nosotros. Descubre los lugares más emblemáticos de la ciudad, pero también explora los barrios menos turísticos para conocer la vida cotidiana de sus habitantes. Disfruta de la deliciosa gastronomía catalana en los restaurantes más auténticos. Nuestros guías locales te contarán historias y anécdotas sobre la ciudad y su cultura, y te llevarán a los rincones más especiales y escondidos que solo conocen los locales. Desde la arquitectura modernista de Gaudí hasta los monumentos históricos de la ciudad, no te pierdas esta experiencia única para conocer Barcelona desde adentro.
-            </Card.Text>
+            <Card.Text className='h4 text-md-right text-center'>{stateInfo.title}</Card.Text>
+            <Card.Text className='text-md-right'>{stateInfo.description}</Card.Text>
           </div>
         </div>
       </Card.Body>
-    </Card>
+      </Card>
+    </div>
 
 
   )
